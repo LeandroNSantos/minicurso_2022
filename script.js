93e9cbe81aec829d482f31b5a928b5628bc27610
Leandro <ln10santos10@gmail.com>
2022-01-16 11:36:05 -0300

    Capturar elementos html manipuláveis

---
+++ script.js
@@ -0,0 +1,5 @@
+const imagem = document.querySelector('img'); /*Cria constante para receber a imagem*/
+const botao = document.querySelector('button'); /*Cria constante para receber o botao*/
+const nomeDoPersonagem = document.querySelector('#nome'); /*Captura a li do nome*/
+const nomeDoPersonagem = document.querySelector('#especie'); /*Captura a li da especie*/
+const nomeDoPersonagem = document.querySelector('#condicao'); /*Captura a li da condicao*/
