909634d532885a12db26c99f5f748aea28ba8a28
Leandro <ln10santos10@gmail.com>
2022-01-16 12:59:02 -0300

    Ajustado o botao

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 const imagem = document.querySelector('img'); /*Cria constante para receber a imagem*/
-const botao = document.querySelector('button'); /*Cria constante para receber o botao*/
+const botao = document.querySelector('botao'); /*Cria constante para receber o botao*/
 const nomeDoPersonagem = document.querySelector('#nome'); /*Captura a li do nome*/
 const nomeDoPersonagem = document.querySelector('#especie'); /*Captura a li da especie*/
 const nomeDoPersonagem = document.querySelector('#condicao'); /*Captura a li da condicao*/
+
+gerarValorAleatorio = () =>{
+    return Math.floor(Math.random() * 671);
+}
+
+/* aerow funcion*/
+pegarPersonagem = () => {
+    let numeroAleatorio = gerarValorAleatorio();
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`,{
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    
+    }).then((response) => response.json()).then((data) => {
+        imagem.src = data.image;
+        imagem.alt = data.name;
+        nomeDoPersonagem.innerHTML = data.name;
+    }); /* retorna os dados da API */
+}
+botao.onclick = pegarPersonagem;
